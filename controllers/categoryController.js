512f4f195cd09cbb2e6a81ab4768fedc6e5eada2
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-09 14:59:54 +0100

    Add the router handler for category creation

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -23,3 +23,39 @@ exports.open_category = asyncHandler(async (req, res) => {
 		items: itemsInCategory,
 	});
 });
+
+//Create category - show form
+exports.show_form = asyncHandler(async (req, res) => {
+	res.render('category_form', { title: 'Create category' });
+});
+//Create
+exports.create_category = [
+	//Validate and sanitize
+	body('name', 'Name must not be empty.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('description', 'Description must not be empty.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+
+	asyncHandler(async (req, res) => {
+		const errors = validationResult(req);
+
+		const category = new Category({
+			name: req.body.name,
+			description: req.body.description,
+		});
+
+		if (!errors.isEmpty()) {
+			res.render('category_form', {
+				title: 'Create category',
+				category: category,
+			});
+		} else {
+			await category.save();
+			res.redirect(category.url);
+		}
+	}),
+];
