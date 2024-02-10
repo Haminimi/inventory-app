c76116e442e4c03e7762b1e2b6195308e11962fd
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-10 01:43:23 +0100

    Add the image field to the category schema

--- models/category.js
+++ models/category.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
 	name: { type: String, required: true },
 	description: { type: String, required: true },
+	image: String,
 });
 
 CategorySchema.virtual('url').get(function () {
